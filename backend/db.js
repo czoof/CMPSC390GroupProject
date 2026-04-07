4bbba4b14ecf6a348e4631c23cffe2cf18e27d39
Alberto <esparza009@sxu.edu>
2026-04-07 12:12:41 -0500

    removed png added to gitignore Additionaly options refresh and cant be reclicked

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",//put your MySQL root password here if you set one
-  database: "legautocustDB"
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
 });
 
 db.connect((err) => {
