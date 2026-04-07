526958f34d74833cfa8d741d801afe069ade1548
Charles Zufan <charliezufan15z@gmail.com>
2026-04-06 21:47:42 -0500

    Update

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,11 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "zoof",//put your MySQL root password here if you set one
-  database: "legautocustDB"
+  host: process.env.DB_HOST || process.env.MYSQLHOST || "localhost",
+  user: process.env.DB_USER || process.env.MYSQLUSER || "root",
+  password: process.env.DB_PASSWORD || process.env.MYSQLPASSWORD || "zoof", // put your MySQL root password here if you set one
+  database: process.env.DB_NAME || process.env.MYSQLDATABASE || "legautocustDB",
+  port: Number(process.env.DB_PORT || process.env.MYSQLPORT || 3306)
 });
 
 db.connect((err) => {
